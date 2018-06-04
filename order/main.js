02c57f11a97e1949208a9bd4b0689760ae68e3d4
MBM <vegomikebriggs@gmail.com>
2018-06-04 14:13:11 +1000

    Many bug fixes.  Now working properly.  Now different length of numbers to order, some formatting changes, added ascending/descending options, etc.

--- order/main.js
+++ order/main.js
@@ -1,8 +1,30 @@
 the_answer = [];
 the_numbers = [];
-their_answer = [];
+ans1 = null;
+ans2 = null;
+ans3 = null;
+ans4 = null;
+ans5 = null;
+num_lower = 0;
+num_upper = 20;
+updown = [];
+amount_of_numbers = 3;
+var score = 0;
 
 
+correct_sequence = function() {
+    playcorrect();  
+    score +=1;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("event").style.color = "green";
+    document.getElementById("event").innerHTML = "You are correct!";
+    document.getElementById("answer1").value = '';
+    document.getElementById("answer2").value = '';
+    document.getElementById("answer3").value = '';
+    document.getElementById("answer4").value = '';
+    document.getElementById("answer5").value = '';
+    generate_a_question();
+}
 var CORRECT_CLIPS = 19;
 
 playcorrect = function() {
@@ -29,30 +51,31 @@ rand_int = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-var score = 0;
+
 
 document.getElementById("score").innerHTML = score;
 
 check_answer = function () {
-    
-    x = +document.getElementById("answer1").value;
-    y = +document.getElementById("answer2").value;
-    z = +document.getElementById("answer3").value;
-    their_answer.push(x,y,z);
-    
-    document.getElementById("cheat-answer").innerHTML = the_answer; 
-    document.getElementById("cheat-guess").innerHTML = their_answer;     
-    
-    
-    //cannot for the life of me work out why this won't work.  Converted them both to a string, they look the same, but still won't compare as equal to each other.  Why?//
-    if (their_answer === the_answer) {
-        playcorrect();
-        score +=1;
-        document.getElementById("score").innerHTML = score;
-        document.getElementById("event").style.color = "green";
-        document.getElementById("event").innerHTML = "You are correct!";
+    v = document.getElementById("answer1").value;
+    w = document.getElementById("answer2").value;
+    x = document.getElementById("answer3").value;
+    y = document.getElementById("answer4").value;
+    z = document.getElementById("answer5").value;
+
+    if (v == ans1 && w == ans2 && x == ans3) {
+        if (amount_of_numbers ==3) {
+            correct_sequence();
+        }
         
-        generate_a_question();
+        else if (amount_of_numbers == 4 && y == ans4) {
+            correct_sequence();
+        }
+            
+        else if (amount_of_numbers == 5 && z == ans5) {
+            correct_sequence();
+        }    
+
+        
     }
     else {
         score -=1;
@@ -74,25 +97,71 @@ answer_keydown = function(event) {
 
 //for whatever reason, all of this when placed the the 'generate_a_question' function and this is deleted causes an error.  No idea why//
 generate_random_numbers = function() {
-    num_lower = 0
-    num_upper = 20
+    the_numbers = [];
     a = rand_int(num_lower, num_upper);
     b = rand_int(num_lower, num_upper);
     c = rand_int(num_lower, num_upper);
     document.getElementById("number-1").innerHTML = a;
     document.getElementById("number-2").innerHTML = b;
     document.getElementById("number-3").innerHTML = c;
-    
     the_numbers.push(a,b,c);
-    document.getElementById("cheat-numbers").innerHTML = the_numbers;
-    the_answer = the_numbers.sort((a, b) => a - b); //ascending  (switch b and a for descending//
+    if (amount_of_numbers == 4) {
+        the_numbers = [];
+        d = rand_int(num_lower, num_upper);
+        document.getElementById("number-4").innerHTML = d;
+        the_numbers.push(a,b,c,d);
+    }
+
+     if (amount_of_numbers == 5) {
+        the_numbers = [];
+        d = rand_int(num_lower, num_upper);
+        e = rand_int(num_lower, num_upper);
+        document.getElementById("number-4").innerHTML = d;
+        document.getElementById("number-5").innerHTML = e;
+        the_numbers.push(a,b,c,d, e);
+    }   
     
-    document.getElementById("cheat-answer").innerHTML = the_answer; 
-    document.getElementById("cheat-guess").innerHTML = their_answer; 
+    the_answer = the_numbers.sort((a, b) => a - b);
+    if (direction == "up") {
+        ans1 = the_answer[0];
+        ans2 = the_answer[1];
+        ans3 = the_answer[2];
+        ans4 = the_answer[3];
+        ans5 = the_answer[4];
+        document.getElementById("instructions").innerHTML = "Put the following numbers in ASCENDING order:";
+    }
+    else if (direction == "down") {
+        if (amount_of_numbers == 3) {    
+            ans1 = the_answer[2];
+            ans2 = the_answer[1];
+            ans3 = the_answer[0];
+            
+        }
+        else if (amount_of_numbers == 4) {
+            ans1 = the_answer[3];
+            ans2 = the_answer[2];
+            ans3 = the_answer[1];
+            ans4 = the_answer[0];
+        }
+        else if (amount_of_numbers == 5) {
+            ans1 = the_answer[4];
+            ans2 = the_answer[3];
+            ans3 = the_answer[2];
+            ans4 = the_answer[1];
+            ans5 = the_answer[0];
+        }
+        document.getElementById("instructions").innerHTML = "Put the following numbers in DESCENDING order:";
+        
+        
+        
+    }
+
+
 }
 
 generate_a_question = function() {
-    generate_random_numbers();
+
+    
     question_difficulty = 0;
     difficulty_radiobuttons = document.getElementsByName("question-difficulty");
     for (var i = 0, length = difficulty_radiobuttons.length; i < length; i++) {
@@ -102,31 +171,83 @@ generate_a_question = function() {
         }
     }
     
-
+    
+    
+    question_type = 0; // a bit of base2 magic...
+    if (document.getElementById("type-up").checked) {
+        //alert('include addition');
+        question_type = question_type + 1;
+    }
+    if (document.getElementById("type-down").checked) {
+        //alert('include subtraction');
+        question_type = question_type + 2;
+    }
+    if (question_type == 0) {
+        alert("Please pick a question type!");
+    }
+    if (question_type == 1) {
+        // addition
+        var directions = ["up"];
+    }
+    else if (question_type == 2) {
+        // subtraction
+        var directions = ["down"];
+    }
+    else if (question_type == 3) {
+        // addition and subtraction
+        var directions = ["down", "up"];
+    }
+    
+    direction = directions[Math.floor(Math.random() * directions.length)];
+    
     if (question_difficulty == 1) {
         num_lower = 1;
         num_upper = 20;
+        amount_of_numbers = 3;
         
     }
     else if (question_difficulty == 2) {
         num_lower = 1;
         num_upper = 50;
+        amount_of_numbers = 4;
         
     }
     else if (question_difficulty == 3) {
-        num_lower = 1;
-        num_upper = 99;
+        num_lower = -20;
+        num_upper = 20;
+        amount_of_numbers = 4;
         
     }
     else if (question_difficulty == 4) {
+        amount_of_numbers = 5;
         num_lower = -100;
         num_upper = 100;
         
     }
 
 
-
     
+    
+
+    if (amount_of_numbers == 3) {
+    document.getElementById("answer4").style.display = "none";
+    document.getElementById("answer5").style.display = "none";
+}
+
+    else if (amount_of_numbers == 4) {
+        document.getElementById("answer4").style.display = "block";
+        document.getElementById("answer5").style.display = "none";
+    }
+
+    else if (amount_of_numbers == 5) {
+        document.getElementById("answer4").style.display = "block";
+        document.getElementById("answer5").style.display = "block";
+    }    
+    
+    generate_random_numbers();
+    document.getElementById("cheat-answer").innerHTML = the_answer; 
+    document.getElementById("cheat-guess").innerHTML = ans3; 
+    document.getElementById("cheat-numbers").innerHTML = ans2;  
     
     
     document.getElementById("answer1").focus();
@@ -164,6 +285,8 @@ document.getElementById("submit").onclick = check_answer;
 document.getElementById("answer1").onkeydown = answer_keydown;
 document.getElementById("answer2").onkeydown = answer_keydown;
 document.getElementById("answer3").onkeydown = answer_keydown;
+document.getElementById("answer4").onkeydown = answer_keydown;
+document.getElementById("answer5").onkeydown = answer_keydown;
 
 // -- start by generating a question
 generate_a_question();
